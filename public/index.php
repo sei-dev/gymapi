25da6027af279d9d2f538b6099b8c39b9a12a47a
Nikola <nikola.bojovic9@gmail.com>
2024-10-02 19:18:30 +0200

    123

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ error_reporting(E_ALL);
 //chdir(__DIR__);
 
 require_once(__DIR__.'/../vendor/APISDK/loader.php');
+require_once(__DIR__.'/../vendor/autoload.php');
 
 set_error_handler(function ($severity, $message, $file, $line) {
 	if (!(error_reporting() & $severity)) {
