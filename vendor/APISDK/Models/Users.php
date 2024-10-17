492d017bd677b6890415dcee735eb5680f1bd3d2
Nikola <nikola.bojovic9@gmail.com>
2024-10-17 17:59:27 +0200

    123

--- vendor/APISDK/Models/Users.php
+++ vendor/APISDK/Models/Users.php
@@ -224,7 +224,7 @@ class Users extends ModelAbstract implements ModelInterface
 	public function getConnectedUsersByTrainerId(string $trainer_id){
 	    
 	    $sQuery = "SELECT users.id, connections.id as connection_id, users.first_name, users.last_name, users.phone, users.email, 
-                   cities.city as location, connections.training_no, connections.debt, connections.price, connections.connected_since 
+                   cities.city as location, connections.training_no, connections.profit, connections.debt, connections.price, connections.connected_since 
                    FROM connections LEFT JOIN users ON users.id = connections.client_id 
                    LEFT JOIN cities ON users.city_id = cities.id
                    WHERE connections.trainer_id = {$trainer_id} AND connections.accepted = '1';
