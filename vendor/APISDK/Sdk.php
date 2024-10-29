0064221ba680fef259d090eccba3d9d8a679ddec
Nikola <nikola.bojovic9@gmail.com>
2024-10-29 16:05:38 +0100

    123

--- vendor/APISDK/Sdk.php
+++ vendor/APISDK/Sdk.php
@@ -1260,7 +1260,10 @@ class Sdk extends Api
      */
     private function sendNotification(string $title, string $body, string $device_token)
     {
-        $client = new Client(__DIR__ . '/personalni-trener-440e6-firebase-adminsdk-vjod3-61b9d09dcc.json');
+        $base64Content = file_get_contents(__DIR__ . '/personalni-trener-440e6-firebase-adminsdk-vjod3-61b9d09dcc.json');
+        $jsonContent = base64_decode($base64Content);
+        
+        $client = new Client($jsonContent);
         // personalni-trener-440e6-firebase-adminsdk-vjod3-61b9d09dcc.json
         $recipient = new Recipient();
         $notification = new Notification();
