135d87ff3e3eaeeed4aa7b05408ee85f669b2416
Nikola <nikola.bojovic9@gmail.com>
2024-11-14 15:38:51 +0100

    123

--- vendor/APISDK/Sdk.php
+++ vendor/APISDK/Sdk.php
@@ -80,7 +80,8 @@ class Sdk extends Api
             'addFitnessCenter',
             'removeFitnessCenter',
             'saveServicesTrainer',
-            'removeInactive'
+            'removeInactive',
+            'initPayment'
         ])) {
             $at = null;
             if (! is_null($this->getBearerToken())) {
@@ -1094,9 +1095,85 @@ class Sdk extends Api
         $connector_api_key = "genericmerchant-simulator-1";
         $connector_shared_secret = "hGa9LECHy2nP7LvHcJI5xbsHtUIIqv";
         $client = new ExchangeClient($api_user, $api_password, $connector_api_key, $connector_shared_secret);
+        
+        
+        // define relevant objects
+        $customer = new Customer();
+        $customer
+        ->setFirstName('John')
+        ->setLastName('Smith')
+        ->setEmail('john@smith.com')
+        ->setIpAddress('123.123.123.123');
+        //add further customer details if necessary
+        
+        // define your transaction ID
+        // must be unique! e.g.
+        $merchantTransactionId = $merchantTransactionId = uniqid('myId', true) . '-' . date('YmdHis');
+        
+        // define transaction relevant object
+        $debit = new Debit();
+        $debit->setMerchantTransactionId($merchantTransactionId)
+        ->setAmount(9.99)
+        ->setCurrency('EUR')
+        ->setCallbackUrl('https://myhost.com/path/to/my/callbackHandler')
+        ->setSuccessUrl('https://myhost.com/checkout/successPage')
+        ->setErrorUrl('https://myhost.com/checkout/errorPage')
+        ->setDescription('One pair of shoes')
+        ->setCustomer($customer);
+        
+        //if token acquired via payment.js
+        if (isset($token)) {
+            $debit->setTransactionToken($token);
+        }
+        
+        //for recurring transactions
+        if ($isRecurringTransaction) {
+            $debit->setReferenceTransactionId($referenceIdFromFirstTransaction);
+        }
+        
+        $result = $client->debit($debit);
+        
+        // handle the result
+        if ($result->isSuccess()) {
+            
+            // store the uuid you receive from the gateway for future references
+            $gatewayReferenceId = $result->getUuid();
+            
+            // handle result based on it's returnType
+            if ($result->getReturnType() == Result::RETURN_TYPE_ERROR) {
+                //error handling
+                $errors = $result->getErrors();
+                // handle the error
+                //cancelCart();
+                
+            } elseif ($result->getReturnType() == Result::RETURN_TYPE_REDIRECT) {
+                //redirect the user
+                header('Location: '.$result->getRedirectUrl());
+                
+            } elseif ($result->getReturnType() == Result::RETURN_TYPE_PENDING) {
+                //payment is pending, wait for callback to complete
+                
+                // handle pending
+                //setCartToPending();
+                
+            } elseif ($result->getReturnType() == Result::RETURN_TYPE_FINISHED) {
+                //payment is finished, update your cart/payment transaction
+                
+                //finishCart();
+            }
+            
+        } else{
+            
+            // handle error
+            // $result->getErrorMessage()
+            // $result->getErrorCode()
+            // $result->getAdapterMessage()
+            // $result->getAdapterCode()
+            
+        }
 
 
-        return $this->formatResponse(self::STATUS_SUCCESS, "", $response);
+        return $this->formatResponse(self::STATUS_SUCCESS, "", $gatewayReferenceId);
     }
 
     private function saveImageReport(String $base64_string, String $file_name, String $dir, String $report_id)
