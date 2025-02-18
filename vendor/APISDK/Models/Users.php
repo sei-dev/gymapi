5c16ee231247a0023f3309b78b926ad34cf8a149
Nikola <nikola.bojovic9@gmail.com>
2025-02-18 10:20:37 +0100

    123

--- vendor/APISDK/Models/Users.php
+++ vendor/APISDK/Models/Users.php
@@ -61,7 +61,7 @@ class Users extends ModelAbstract implements ModelInterface
         ->query($sQuery)
         ->fetch(\PDO::FETCH_ASSOC);
         if (isset($row)) {
-            return $row[0]["price"];
+            return $row;
         }
         return false;
     }
