b7271a585a3feec431a59346371169d891d4e671
Nikola <nikola.bojovic9@gmail.com>
2025-02-24 16:55:39 +0100

    123

--- vendor/APISDK/Sdk.php
+++ vendor/APISDK/Sdk.php
@@ -160,7 +160,7 @@ class Sdk extends Api
 
         array_walk($trainings, function (&$a) {
             if ($this->isFileExists(self::DIR_USERS, $a["client_id"])) {
-                $a['image'] = $this->domain . "/images/users/" . $a["client_id"] . ".png?r=" . rand(0, 100000);
+                $a['image'] = $this->domain . "/images/users/" . $a["client_ids"] . ".png?r=" . rand(0, 100000);
             } else {
                 $a['image'] = $this->domain . "/images/users/logo.png";
             }
@@ -193,7 +193,7 @@ class Sdk extends Api
 
         array_walk($trainings, function (&$a) {
             if ($this->isFileExists(self::DIR_USERS, $a["client_id"])) {
-                $a['image'] = $this->domain . "/images/users/" . $a["client_id"] . ".png?r=" . rand(0, 100000);
+                $a['image'] = $this->domain . "/images/users/" . $a["client_ids"] . ".png?r=" . rand(0, 100000);
             } else {
                 $a['image'] = $this->domain . "/images/users/logo.png";
             }
