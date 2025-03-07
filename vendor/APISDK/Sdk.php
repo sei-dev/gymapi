06d621f1ec17be7ec458b9f9228f5c943692e046
Nikola <nikola.bojovic9@gmail.com>
2025-03-07 08:14:11 +0100

    123

--- vendor/APISDK/Sdk.php
+++ vendor/APISDK/Sdk.php
@@ -1833,8 +1833,8 @@ class Sdk extends Api
     
     private function testPing(){
     
-        $netRacuni = new NetRacun('net_racuni_staging_e3gOhLmkSIeL5WtW18PGlkfZxwIfK2upy1HDvMNL378aaffe');
-        $netRacuni->sandbox();
+        $netRacuni = new NetRacun('net_racuni_e3gOhLmkSIeL5WtW18PGlkfZxwIfK2upy1HDvMNL378aaffe');
+        //$netRacuni->sandbox();
         
         $result = $netRacuni->ping();
     
