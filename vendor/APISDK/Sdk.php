9125501f422a3f69ed76c89201b13bdbc2b35bb1
Nikola <nikola.bojovic9@gmail.com>
2025-05-08 17:36:00 +0200

    123

--- vendor/APISDK/Sdk.php
+++ vendor/APISDK/Sdk.php
@@ -1876,8 +1876,8 @@ class Sdk extends Api
     
     private function testInvoiceCheck(){
         
-        $netRacuni = new NetRacun('net_racuni_e3gOhLmkSIeL5WtW18PGlkfZxwIfK2upy1HDvMNL378aaffe');
-        //$netRacuni->sandbox();
+        $netRacuni = new NetRacun('net_racuni_staging_YgbuxF1Le0Y9KavjUnKoHeCGivlnXlCY4p5iHGju8480dec3');
+        $netRacuni->sandbox();
         
         //NetRacunResponse
         $items = [
@@ -1885,7 +1885,7 @@ class Sdk extends Api
                 [
                     "name" => "Test Item",
                     "taxLabels" => [
-                        "A"
+                        "Ж"
                     ],
                     "unit" => "KOM",
                     "quantity" => 2,
