a081f1d388ea435d133bbe0ed33a38a2b3ef01ba
Nikola Bojovic <nikola.bojovic9@gmail.com>
2025-11-16 18:40:43 +0100

    123

--- vendor/APISDK/Sdk.php
+++ vendor/APISDK/Sdk.php
@@ -1395,8 +1395,13 @@ class Sdk extends Api
         
         //return $this->formatResponse(self::STATUS_SUCCESS, $connection, $users);
         
+        // normalize the first user into array
+        $users[0] = (array) $users[0];
+        
+        // convert array → object safely
         $userObject = (object) $users[0];
         
+        // now these work without errors
         $userToReturn = $this->returnUser((array)$userObject);
         $userToReturn['connection'] = $connection;
         
